7db6f7816e60267eb53b041b4ef7e53dc4a440cc
Lisney <lisneyborroto@gmail.com>
2020-03-13 13:54:44 -0500

    add economy scraper
    folder created for economy including an HTML file

---
+++ findrcity/data_collection/economy_scraper/economy_scraper.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+from bs4 import BeautifulSoup
+
+
+# Remove city names after comma, dash, or slash
+def get_city(data):
+    data = data.split("-")[0]
+    data = data.split(",")[0]
+    data = data.split("/")[0]
+    return data
+
+
+def get_economy_data():
+    soup = BeautifulSoup(open("unemployment_table.html"), "html.parser")
+    data = defaultdict()
+
+    for row in soup.select('tr'):
+        city = get_city(row.p.text)
+        row_cells = [cell for cell in row.select('td')]
+        data[city] = float(row_cells[0].text)
+
+    return data
