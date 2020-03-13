cfafd7c871a618560562b2fe8c5e3bb658dee731
Lisney <lisneyborroto@gmail.com>
2020-03-13 13:17:33 -0500

    fix population data script
    was incorrectly placed in weather data

--- findrcity/city_list/data_loaders/load_weather_data.py
+++ findrcity/city_list/data_loaders/load_weather_data.py
@@ -3,17 +3,6 @@ from city_list.models import City, State
 from data_collection.weather_scraper import get_weather_data
 from data_collection.population_scraper import get_population_data
 
-def load_population_data():
-    population_data = get_population_data()
-
-    for record in population_data:
-        if City.objects.filter(name=record).exists():
-            city_instance = City.objects.get(name=record)
-            city_instance.population = population_data[record]
-            city_instance.save()
-        else:
-            continue
-
 def load_weather_data():
     weather_data = get_weather_data()
 
