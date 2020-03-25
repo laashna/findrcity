84dd8fad51674a9355571bad1ce8f00c9aaf76f0
nicole <nam6624@louisiana.edu>
2020-03-25 16:20:10 -0500

    add city name and high/low temp into citylist table

--- findrcity/city_list/views.py
+++ findrcity/city_list/views.py
@@ -1,5 +1,10 @@
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from city_list.models import City
 
 
-class CityListView(TemplateView):
+class CityListView(ListView):
     template_name = 'city_list/city_list.html'
+    model = City
+    context_object_name = 'city'
