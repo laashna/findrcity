eb77c3c240c44ab03bff4944942b438b7772ccf4
nicole <nam6624@louisiana.edu>
2020-03-25 16:59:37 -0500

    add city_list test

--- findrcity/city_list/tests.py
+++ findrcity/city_list/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestCityList(TestCase):
+    def test_call_city_list(self):
+        response = self.client.get(reverse('city_list'), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'city_list/city_list.html')
