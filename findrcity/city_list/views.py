1debeb941e931fb582865ae90268f9e15eac4f0f
Lisney <lisneyborroto@gmail.com>
2020-03-25 18:33:26 -0500

    change category filter to use dictionary

--- findrcity/city_list/views.py
+++ findrcity/city_list/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
+from contexts.city_list_contexts import FILTER_CATEGORIES_CONTEXT
 
 
 class CityListView(TemplateView):
     template_name = 'city_list/city_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CityListView, self).get_context_data(**kwargs)
+        context['category'] = FILTER_CATEGORIES_CONTEXT
+        return context
---
+++ findrcity/contexts/city_list_contexts.py
@@ -0,0 +1,27 @@
+FILTER_CATEGORIES_CONTEXT = {
+    'Weather': {
+        'name': 'Weather',
+        'collapse': 'collapseWeather',
+        'body': 'text of body'
+    },
+    'Crime': {
+        'name': 'Crime',
+        'collapse': 'collapseCrime',
+        'body': 'text of body'
+    },
+    'Population': {
+        'name': 'Population',
+        'collapse': 'collapsePopulation',
+        'body': 'text of body'
+    },
+    'Transportation': {
+        'name': 'Transportation',
+        'collapse': 'collapseTransportation',
+        'body': 'text of body'
+    },
+    'Economics': {
+        'name': 'Economics',
+        'collapse': 'collapseEconomics',
+        'body': 'text of body'
+    }
+}
