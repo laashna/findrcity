a2d87871345eb63d3317000f3b1caff660081e83
Lisney <lisneyborroto@gmail.com>
2020-03-25 21:10:46 -0500

    change filter to expand on heading selection

--- findrcity/contexts/city_list_contexts.py
+++ findrcity/contexts/city_list_contexts.py
@@ -1,27 +1,17 @@
 FILTER_CATEGORIES_CONTEXT = {
     'Weather': {
-        'name': 'Weather',
-        'collapse': 'collapseWeather',
         'body': 'text of body'
     },
     'Crime': {
-        'name': 'Crime',
-        'collapse': 'collapseCrime',
         'body': 'text of body'
     },
     'Population': {
-        'name': 'Population',
-        'collapse': 'collapsePopulation',
         'body': 'text of body'
     },
     'Transportation': {
-        'name': 'Transportation',
-        'collapse': 'collapseTransportation',
         'body': 'text of body'
     },
     'Economics': {
-        'name': 'Economics',
-        'collapse': 'collapseEconomics',
         'body': 'text of body'
     }
 }
