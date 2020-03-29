6aff5e4317b4bf1ad8d8da6366857495bd1337d9
Aashna Lakhani <c00220036@louisiana.edu>
2020-03-29 00:01:37 -0500

    update dimensions and position of map

--- findrcity/static/js/findrcity.js
+++ findrcity/static/js/findrcity.js
@@ -61,7 +61,7 @@
   });
 
   //MAP!!
-  var mymap = L.map("city-map").setView([39.381266, -97.922211], 3);
+  var mymap = L.map("city-map").setView([39.381266, -97.922211], 4);
 
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
