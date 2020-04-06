fae4e6601b09964c932011ad505010dbc3e83fa4
Lisney <lisneyborroto@gmail.com>
2020-04-06 18:12:16 -0500

    change URL from city to city_detail

--- findrcity/city_detail/urls.py
+++ findrcity/city_detail/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from city_detail.views import CityDetailView
 
 urlpatterns = [
-    path('<str:name>', CityDetailView.as_view(), name="city")
+    path('<str:name>', CityDetailView.as_view(), name="city_detail")
 ]
