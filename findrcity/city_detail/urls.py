5399ecf393922c8f4cd76ab7018e666d6feea293
Al Sharairi <asharairi@gmail.com>
2020-04-06 21:25:04 -0500

    Merge master into 15

--- findrcity/city_detail/urls.py
+++ findrcity/city_detail/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from city_detail.views import CityDetailView
 
 urlpatterns = [
-    path('', CityDetailView.as_view(), name='city_detail')
+    path('<str:name>', CityDetailView.as_view(), name="city")
 ]
--- findrcity/city_list/views.py
+++ findrcity/city_list/views.py
@@ -1,11 +1,17 @@
 from django.views.generic import TemplateView
-from contexts.city_list_contexts import FILTER_CATEGORIES_CONTEXT
+from contexts.city_list_contexts import FILTER_CATEGORIES_CONTEXT, CITY_LIST_HEADERS_CONTEXT
+from django.views.generic import ListView
+
+from city_list.models import City
 
 
-class CityListView(TemplateView):
+class CityListView(ListView):
     template_name = 'city_list/city_list.html'
+    model = City
+    context_object_name = 'cities'
 
     def get_context_data(self, **kwargs):
         context = super(CityListView, self).get_context_data(**kwargs)
         context['category'] = FILTER_CATEGORIES_CONTEXT
+        context['city_list_headers'] = CITY_LIST_HEADERS_CONTEXT
         return context
