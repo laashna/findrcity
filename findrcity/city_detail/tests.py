65c37a1bf1a56a5b8ffe1bffc5cb1418ff65aee4
nicole <nam6624@louisiana.edu>
2020-04-06 20:04:54 -0500

    add city detail test

--- findrcity/city_detail/tests.py
+++ findrcity/city_detail/tests.py
@@ -1,9 +1,16 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from city_list.models import State, City
+
 
 class TestCityDetail(TestCase):
-    def test_call_city_detail(self):
-        response = self.client.get(reverse('city_detail'), follow=True)
+    @classmethod
+    def setUpTestData(cls):
+        State.objects.create(name='Louisiana')
+        City.objects.create(name='Lafayette', temperature_high=100, state=State.objects.get(name='Louisiana'))
+
+    def test_city_detail_url_config(self):
+        response = self.client.get(reverse('city_detail', args=['Lafayette']), follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'city_detail/city_detail.html')
